5888447bef3625612bd1072a63b4ade25d1d68fe
rushike5h02 <121027173+rushike5h02@users.noreply.github.com>
2023-01-10 21:12:49 +0530

    Add files via upload
    All the Assignment

---
+++ AssignmentOne.java
@@ -0,0 +1,11 @@
+
+public class AssignmentOne {  // <access specifier> <Class> <ClassName>
+	
+	
+	public static void main(String[] args) {  // <accss specifier> <return type> main Class ()
+		
+		System.out.println("Velocity Corporate Training Centre");
+		
+	}
+
+}
